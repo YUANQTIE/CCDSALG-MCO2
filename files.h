042e3290d27ca7b19d199b7165360b9319776f01
ay3s4 <ayesatullao@gmail.com>
2025-07-20 17:25:08 +0800

    Update files.h
    Added Output5 and Output6 Function Declaration

--- files.h
+++ files.h
@@ -9,5 +9,6 @@ void Output2(char* fileName, Graph graph);
 void Output3(char* fileName, Graph graph, char line[][1000]);
 void Output4(char* fileName, Graph graph);
 void Output5(char* fileName, Graph graph, char start[100]) ;
+void Output6(char* fileName, Graph graph, char start[100]);
 void Output7(char* fileName, char* fileName1, char* fileName2, Graph graph1, Graph graph2);
 #endif
