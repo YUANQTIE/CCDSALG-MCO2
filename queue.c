df8a1a5e0618d356544e82d905787e74fa156c48
YUANQTIE <yuanpanlilio@gmail.com>
2025-07-24 00:20:35 +0800

    Update queue.c

--- queue.c
+++ queue.c
@@ -12,10 +12,19 @@ Princess Ayesa A. Tullao, 12412112
 #include <string.h>
 #include "queue.h"
 
+/*Function that initializes the queue indices.
+	@param queue - the queue whose indicies will be initialized.
+*/
+
 void Create(Queue *queue){
     queue->front = 0;
     queue->rear = 0;
 }
+
+/*Function that enqueues an entry to the rear of the queue.
+	@param queue - the queue where the entry will be enqueued in.
+    @param entry - the entry to be added to the queue.
+*/
 
 void Enqueue(Queue *queue, char entry[]){
     if (QueueFull(queue)){
@@ -27,43 +36,55 @@ void Enqueue(Queue *queue, char entry[]){
     }
 }
 
+/*Function that dequeues an entry from the head of the queue.
+	@param queue - the queue whose head will be dequeued.
+    @return value at the head of the queue.
+*/
+
 char* Dequeue(Queue *queue){
+    char* value = NULL;
     if (QueueEmpty(queue)){
         printf("Underflow.\n");
-        return NULL;
     }
     else{
         queue->front++;
-        return queue->entry[queue->front - 1];
+        value = queue->entry[queue->front - 1];
     }
+
+    return value;
 }
 
+/*Function that allows to get a peek of the value at the head of the queue.
+	@param queue - the queue whose head will be checked.
+    @return value at the head of the queue.
+*/
 
 char* QueueHead(Queue *queue){
-
+    char* value = NULL;
     if (QueueEmpty(queue)){
         printf("Underflow.\n");
-        return NULL;
     }
     else{
-        return queue->entry[queue->front]; 
+        value = queue->entry[queue->front]; 
     }
+
+    return value;
 }
 
-char* QueueTail(Queue *queue){
-
-    if (QueueEmpty(queue)){
-        printf("Underflow.\n");
-        return NULL;
-    }
-    else{
-        return queue->entry[queue->rear]; 
-    }
-}
+/*Function that allows that checks if the queue is empty.
+	@param queue - the queue whose contents will be checked.
+    @return 1 if the index at the head of the queue is equal to index at the tail of the queue.
+*/
 
 int QueueEmpty(Queue *queue){
     return queue->front == queue->rear;
 }
+
+/*Function that allows that checks if the queue is full.
+	@param queue - the queue whose contents will be checked.
+    @return 1 if the index at the rear is already before the index at the head.
+*/
+
 int QueueFull(Queue *queue){
     return queue->front == (queue->rear + 1) % MAX_VERTICES;
 }
